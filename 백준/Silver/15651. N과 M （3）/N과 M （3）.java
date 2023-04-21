777c8f9d62e52bb6ad7c1eb05481d2d598a586ec
YEO MINJI <43868492+yeomj051@users.noreply.github.com>
2023-04-21 09:11:46 +0900

    [Silver III] Title: N과 M (3), Time: 384 ms, Memory: 111808 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    private static int M;
+    private static int N;
+    private static int[] result;
+    private static StringBuilder stringBuilder = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+
+        N = Integer.parseInt(stringTokenizer.nextToken());
+        M = Integer.parseInt(stringTokenizer.nextToken());
+
+        result = new int[M];
+        logic(0);
+        System.out.println(stringBuilder);
+
+    }
+
+    private static void logic(int count) {
+        if(count == M){
+            for (int i = 0; i < M; i++) {
+                stringBuilder.append(result[i]).append(" ");
+            }
+            stringBuilder.append("\n");
+            return;
+        }
+
+        for (int i = 1; i <= N; i++) {
+            result[count] = i;
+            logic(count+1);
+        }
+    }
+}
+
